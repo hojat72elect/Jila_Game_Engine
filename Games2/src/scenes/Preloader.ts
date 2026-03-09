ad4a46184b66567b10deb33427cc2b101b32d938
hojat72elect <hojat72elect@gmail.com>
2026-03-09 00:43:04 -0400

    Preloader.js -> ts

--- Games2/src/scenes/Preloader.ts
+++ Games2/src/scenes/Preloader.ts
@@ -13,14 +13,12 @@ export class Preloader extends Scene {
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
+        const bar: Phaser.GameObjects.Rectangle = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
-        this.load.on('progress', (progress) => {
-
+        this.load.on('progress', (progress: number) => {
             //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
             bar.width = 4 + (460 * progress);
-
         });
     }
 
@@ -34,7 +32,7 @@ export class Preloader extends Scene {
     }
 
     create() {
-        //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
+        //  When all the assets have been loaded, it's often worth creating global objects here that the rest of the game can use.
         //  For example, we will define our 'coin' animation here, so we can use it in other scenes:
 
         this.anims.create({
@@ -58,7 +56,7 @@ export class Preloader extends Scene {
             target: 'MainMenu',
             duration: 1000,
             moveBelow: true,
-            onUpdate: (progress) => {
+            onUpdate: (progress: number) => {
                 this.cameras.main.setAlpha(1 - progress);
             }
         });
