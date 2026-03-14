f095d3e3b1d3511576e02efff757874978c01d62
hojat72elect <hojat72elect@gmail.com>
2026-03-14 16:55:26 -0400

    Added barebones of the game 8.

---
+++ Games8_flood_fill/src/game/main.ts
@@ -0,0 +1,29 @@
+import {AUTO, Game, Scale, Types} from 'phaser';
+import {Example} from "./part10";
+
+const config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {x: 0, y: 300},
+            debug: false
+        }
+    },
+    parent: 'game-container',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    scene: [
+        Example
+    ]
+};
+
+const StartGame = (parent: string) => {
+    return new Game({...config, parent});
+}
+
+export default StartGame;
