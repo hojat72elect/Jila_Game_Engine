3d84bbf55a6542a181dfaf5b3f61c871d13b57a9
hojat72elect <hojat72elect@gmail.com>
2026-03-13 23:48:51 -0400

    fixed all errors.

--- Games6_emoji_match/src/game/scenes/Preloader.js
+++ Games6_emoji_match/src/game/scenes/Preloader.js
@@ -6,19 +6,18 @@ export default class Preloader extends Phaser.Scene {
     }
 
     preload() {
-        this.load.setBaseURL('https://cdn.phaserfiles.com/v385');
         this.loadText = this.add.text(400, 360, 'Loading ...', {fontFamily: 'Arial', fontSize: 64, color: '#e3f2ed'});
 
         this.loadText.setOrigin(0.5);
         this.loadText.setStroke('#203c5b', 6);
         this.loadText.setShadow(2, 2, '#2d2d2d', 4, true, false);
 
-        this.load.setPath('assets/games/emoji-match/');
+        this.load.setPath('assets/');
         this.load.image(['background', 'logo']);
-        this.load.atlas('emojis', 'emojis.png', 'emojis.json');
+        this.load.multiatlas('emojis', 'emojis.json');
 
         //  Audio ...
-        this.load.setPath('assets/games/emoji-match/sounds/');
+        this.load.setPath('assets/sounds/');
 
         this.load.audio('music', ['music.ogg', 'music.m4a', 'music.mp3']);
         this.load.audio('countdown', ['countdown.ogg', 'countdown.m4a', 'countdown.mp3']);
