3c81b40be393914fd829d2c4e587d012407dcdb6
hojat72elect <hojat72elect@gmail.com>
2026-03-13 22:57:05 -0400

    fixed loading issues.

--- Games5_breakout/src/game/scenes/breakout.js
+++ Games5_breakout/src/game/scenes/breakout.js
@@ -9,7 +9,7 @@ export class Breakout extends Phaser.Scene {
 
     preload() {
         this.load.setBaseURL('assets');
-        this.load.atlas( 'assets/breakout.png', 'assets/breakout.json');
+        this.load.atlas('breakout', 'breakout.png', 'breakout.json');
     }
 
     create() {
@@ -18,15 +18,15 @@ export class Breakout extends Phaser.Scene {
 
         //  Create the bricks in a 10x6 grid
         this.bricks = this.physics.add.staticGroup({
-            key: 'assets', frame: ['blue1', 'red1', 'green1', 'yellow1', 'silver1', 'purple1'],
+            key: 'breakout', frame: ['blue1', 'red1', 'green1', 'yellow1', 'silver1', 'purple1'],
             frameQuantity: 10,
             gridAlign: {width: 10, height: 6, cellWidth: 64, cellHeight: 32, x: 112, y: 100}
         });
 
-        this.ball = this.physics.add.image(400, 500, 'assets', 'ball1').setCollideWorldBounds(true).setBounce(1);
+        this.ball = this.physics.add.image(400, 500, 'breakout', 'ball1').setCollideWorldBounds(true).setBounce(1);
         this.ball.setData('onPaddle', true);
 
-        this.paddle = this.physics.add.image(400, 550, 'assets', 'paddle1').setImmovable();
+        this.paddle = this.physics.add.image(400, 550, 'breakout', 'paddle1').setImmovable();
 
         //  Our colliders
         this.physics.add.collider(this.ball, this.bricks, this.hitBrick, null, this);
