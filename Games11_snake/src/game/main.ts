812b882ecd027a4447d382264867c1018c0b7c26
hojat72elect <hojat72elect@gmail.com>
2026-03-14 22:48:05 -0400

    Added barebones of the snake game.

---
+++ Games11_snake/src/game/main.ts
@@ -0,0 +1,28 @@
+import {AUTO, Game, Scale, Types} from 'phaser';
+import {MainMenu} from "./MainMenu";
+import {ShinePostFX} from "./ShinePostFX";
+import {WipePostFX} from "./WipePostFX";
+import {Preloader} from "./Preloader";
+import {PuzzleGame} from "./Game";
+
+const config: Types.Core.GameConfig = {
+    type: AUTO,
+    width: 1024,
+    height: 768,
+    backgroundColor: '#002157',
+    parent: 'game-container',
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH
+    },
+    scene: [
+        Preloader, MainMenu, PuzzleGame
+    ],
+    pipeline: { ShinePostFX, WipePostFX }
+};
+
+const StartGame = (parent: string) => {
+    return new Game({...config, parent});
+}
+
+export default StartGame;
